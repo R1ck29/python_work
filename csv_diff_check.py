ddee77bb9a6fe0c0bf59bf83e5a4d1310f76dc2d
Riku Suzuki <r1cky.ft9@gmail.com>
2018-06-19 18:12:41 +0900

    Create csv_diff_check.py

---
+++ csv_diff_check.py
@@ -0,0 +1,22 @@
+#! python3
+# coding: utf-8
+
+import pandas as pd
+import os
+import csv
+
+# move to work directory
+os.chdir("C:/tools/CSV")
+
+df01 = pd.read_csv('HUE比較用_賞データ_fsta.csv', engine='python', encoding='Shift-JIS') # HUEに入れたデータ
+df02 = pd.read_csv('HUE_賞データ(f-sta).csv', engine='python', encoding='Shift-JIS') # HUEから取り出したデータ
+
+df01['diff'] = df01[['idou_sya_bg', 'sybtrk_sybt_cd']].apply(lambda x: '{}_{}'.format(x[0], x[1]), axis=1)
+
+df02['diff'] = df02[['idou_sya_bg', 'sybtrk_sybt_cd']].apply(lambda x: '{}_{}'.format(x[0], x[1]), axis=1)
+
+# df01には存在しておらず、df02には存在している行が出る
+df_diff = df01[~df01['diff'].isin(df02['diff'])]
+df_diff.to_csv('diff_result.csv', index=None)
+
+# idou_sya_bg,sybtrk_sybt_cd,sybtrk_sybdate,sybtrk_sybt_jiyu_cd
